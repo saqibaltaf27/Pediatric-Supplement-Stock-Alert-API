42baab053bba22deffafb8052c257d7ae2be7c25
Saqib Altaf <saqibkh1805@gmail.com>
2026-02-02 14:49:30 +0500

    Updated Paths

--- models/db.js
+++ models/db.js
@@ -4,32 +4,32 @@ const path = require("path");
 
 const dbPath = path.resolve("./data/database.sqlite"); 
 const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
-  if (err) console.error("DB connection error:", err.message);
-  else console.log("Connected to SQLite DB (read-only)");
+  if (err) console.error("Failed to open DB:", err.message);
+  else console.log("Connected to SQLite DB");
 });
 
 // Enable foreign keys
-db.run("PRAGMA foreign_keys = ON");
+//db.run("PRAGMA foreign_keys = ON");
 
 // Initialize the Tables
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS Products (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL
-    )
-  `);
+// db.serialize(() => {
+//   db.run(`
+//     CREATE TABLE IF NOT EXISTS Products (
+//       id INTEGER PRIMARY KEY AUTOINCREMENT,
+//       name TEXT NOT NULL
+//     )
+//   `);
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS notifications (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      product_id INTEGER NOT NULL,
-      email TEXT NOT NULL,
-      sent INTEGER DEFAULT 0,
-      UNIQUE(product_id, email),
-      FOREIGN KEY(product_id) REFERENCES Products(id)
-    )
-  `);
-});
+//   db.run(`
+//     CREATE TABLE IF NOT EXISTS notifications (
+//       id INTEGER PRIMARY KEY AUTOINCREMENT,
+//       product_id INTEGER NOT NULL,
+//       email TEXT NOT NULL,
+//       sent INTEGER DEFAULT 0,
+//       UNIQUE(product_id, email),
+//       FOREIGN KEY(product_id) REFERENCES Products(id)
+//     )
+//   `);
+// });
 
 module.exports = db;
